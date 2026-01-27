9e8a28da02b5e3cf2a55b64e96471d32ae55bb5a
Dimas Setia Pambudi <setia.pambudi@bps.go.id>
2026-01-27 10:44:19 +0700

    nvim: remove README and add blankline

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -148,9 +148,9 @@ local plugins = {
       vim.treesitter.language.register("twig", "nunjucks")
       vim.treesitter.language.register("glimmer", "handlebars")
     end,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
+    -- dependencies = {
+    --   "nvim-treesitter/nvim-treesitter-textobjects",
+    -- },
   },
   {
     "kevinhwang91/nvim-ufo",
@@ -319,6 +319,38 @@ local plugins = {
     },
     -- stylua: ignore
     keys = require("mappings").sidekick,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function()
+        local highlight = {
+            "RainbowRed",
+            "RainbowYellow",
+            "RainbowBlue",
+            "RainbowOrange",
+            "RainbowGreen",
+            "RainbowViolet",
+            "RainbowCyan",
+        }
+
+        local hooks = require "ibl.hooks"
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+            vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+            vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+            vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+            vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+            vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+            vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+        end)
+
+        require("ibl").setup { scope = { highlight = highlight } }
+    end,
+    event = "User FilePost",
   },
 }
 
